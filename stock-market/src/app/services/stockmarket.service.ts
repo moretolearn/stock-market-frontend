4f9e1a971d385726b7adcb6fbacd68a499a69656
moretolearn <yanumularammohan661@gmail.com>
2022-05-29 23:20:41 +0530

    Mapped Companies List endpoint

--- stock-market/src/app/services/stockmarket.service.ts
+++ stock-market/src/app/services/stockmarket.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Company } from '../models/company.model';
+import { Stock } from '../models/stock.model';
+import { ApiResponse } from '../models/api-response.model';
+const companyBaseUrl = 'http://localhost:8083/api/v1/company';
+const stockBaseUrl = 'http://localhost:8081/api/v1/stock';
 @Injectable({
   providedIn: 'root'
 })
 export class StockmarketService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+    getAll(): Observable<ApiResponse<Company[]>> {
+      return this.http.get(`${companyBaseUrl}/getall`);
+   }
+   get(id: number): Observable<ApiResponse<Company>> {
+    return this.http.get(`${companyBaseUrl}/info/${id}`);
+  }
+  add(company : Company): Observable<ApiResponse<any>> {
+    return this.http.post(`${companyBaseUrl}/add`,company);
+  }
+  delete(id : number): Observable<ApiResponse<any>> {
+    return this.http.delete(`${companyBaseUrl}/delete/${id}`);
+  }
 }
