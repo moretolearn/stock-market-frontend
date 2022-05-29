509436ce104049245abd42c6e9dd0302d8e047a2
moretolearn <yanumularammohan661@gmail.com>
2022-05-29 23:20:12 +0530

    Added fields for Comapny and Stock

--- stock-market/src/app/models/stock.model.ts
+++ stock-market/src/app/models/stock.model.ts
@@ -1,2 +1,12 @@
+import { Company } from "./company.model";
+
 export class Stock {
+
+    stockCode?: number;
+    stockName?: string;
+    description?: string;
+    price?: number;
+    startDate?: Date;
+    endDate?: Date;
+    company?: Company
 }
