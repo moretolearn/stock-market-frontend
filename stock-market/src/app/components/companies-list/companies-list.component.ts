4d8fe8dea6943089aca325598058006db68156fd
moretolearn <yanumularammohan661@gmail.com>
2022-06-01 08:45:18 +0530

    Added Sorting and Paginations to Company List

--- stock-market/src/app/components/companies-list/companies-list.component.ts
+++ stock-market/src/app/components/companies-list/companies-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { StockmarketService } from 'src/app/services/stockmarket.service';
 
@@ -11,27 +13,40 @@ export class CompaniesListComponent implements OnInit {
 
   companyDataSource = new MatTableDataSource<any>();
 
-  displayedColumns: string[] = ['companyCode', 'companyName', 'ceo', 'turnover','website','exchange','description','actions'];
 
-  constructor(private smService:StockmarketService) { }
+  @ViewChild(MatSort) matSort:MatSort;
+  @ViewChild(MatPaginator) matPaginator:MatPaginator
+
+
+  displayedColumns: string[] = ['companyCode', 'companyName', 'ceo', 'turnover', 'website', 'exchange', 'description', 'actions'];
+
+  constructor(private smService: StockmarketService) { }
 
   ngOnInit(): void {
-this.getCompaniesList()
+    this.getCompaniesList()
   }
 
-  public getCompaniesList(){
-    this.smService.getAll().subscribe(res=>{
-      console.log(res)
+  public getCompaniesList() {
+    this.smService.getAll().subscribe(res => {
+      // console.log(res)
       this.companyDataSource.data = res.result;
+      this.companyDataSource.sort= this.matSort;
+      this.companyDataSource.paginator =  this.matPaginator;
     })
   }
 
-  public onCompanyUpdate(row:any){
+  public onCompanyUpdate(row: any) {
 
   }
 
-  public onCompanyDelete(row:any){
+  public onCompanyDelete(row: any) {
 
+    this.smService.delete(row.companyCode).subscribe({
+      next: (res) => {
+        console.log(res)
+        this.getCompaniesList()
+      },
+    })
   }
 
 }
