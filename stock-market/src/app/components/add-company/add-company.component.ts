6b288e1afb8f721da4d4703d31ce111a1e949835
moretolearn <yanumularammohan661@gmail.com>
2022-06-02 22:56:28 +0530

    Add ompany updated

--- stock-market/src/app/components/add-company/add-company.component.ts
+++ stock-market/src/app/components/add-company/add-company.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { StockmarketService } from 'src/app/services/stockmarket.service';
 
 @Component({
   selector: 'app-add-company',
@@ -7,9 +10,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddCompanyComponent implements OnInit {
 
-  constructor() { }
+  companyForm !: FormGroup;
+  constructor(
+    private formBuilder : FormBuilder,
+    private smService : StockmarketService,
+    private dialogRef : MatDialogRef<AddCompanyComponent>) { }
+
+
 
   ngOnInit(): void {
+    this.companyForm=this.formBuilder.group({
+      companyName : ['',Validators.required],
+      description : ['',Validators.required],
+      ceo : ['',Validators.required],
+      turnover : ['',[Validators.required, Validators.pattern("^[0-9]*$")]],
+      website : ['',Validators.required],
+      exchange : ['', Validators.required]
+
+    })
+
   }
+
+  addCompany(){
+    if(this.companyForm.valid){
+      this.smService.add(this.companyForm.value)
+      .subscribe({
+        next:(res)=>{
+          alert("Company Registered");
+
+          this.companyForm.reset();
+          this.dialogRef.close();
+
+        },
+        error:()=>{
+          alert("Error occured while adding the Company")
+        }
+
+      })
+    }
+
+  }
+
+
 
 }
