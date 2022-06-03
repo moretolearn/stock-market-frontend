cef0d6127b7596a43b2de7e010d177049901212f
moretolearn <yanumularammohan661@gmail.com>
2022-06-04 00:29:24 +0530

    Company List Code Updated

--- stock-market/src/app/components/companies-list/companies-list.component.ts
+++ stock-market/src/app/components/companies-list/companies-list.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
 import { Company } from 'src/app/models/company.model';
 import { StockmarketService } from 'src/app/services/stockmarket.service';
+import { AddCompanyComponent } from '../add-company/add-company.component';
 
 @Component({
   selector: 'app-companies-list',
@@ -16,16 +18,17 @@ export class CompaniesListComponent implements OnInit {
   companyDataSource = new MatTableDataSource<any>();
 
 
-  @ViewChild(MatSort) matSort:MatSort;
-  @ViewChild(MatPaginator) matPaginator:MatPaginator
+  @ViewChild(MatSort) matSort: MatSort;
+  @ViewChild(MatPaginator) matPaginator: MatPaginator
 
 
   displayedColumns: string[] = ['companyCode', 'companyName', 'ceo', 'turnover', 'website', 'exchange', 'description', 'actions'];
 
   constructor(
     private smService: StockmarketService,
-    private router:Router
-    ) { }
+    private router: Router,
+    private dialog: MatDialog
+  ) { }
 
   ngOnInit(): void {
     this.getCompaniesList()
@@ -35,17 +38,12 @@ export class CompaniesListComponent implements OnInit {
     this.smService.getAll().subscribe(res => {
       // console.log(res)
       this.companyDataSource.data = res.result;
-      this.companyDataSource.sort= this.matSort;
-      this.companyDataSource.paginator =  this.matPaginator;
+      this.companyDataSource.sort = this.matSort;
+      this.companyDataSource.paginator = this.matPaginator;
     })
   }
 
-  public onCompanyUpdate(row: any) {
-console.log("hhh");
-console.log(row)
-  }
-
-  public onCompanyDelete(row: any) {
+  public deleteCompany(row: any) {
 
     this.smService.delete(row.companyCode).subscribe({
       next: (res) => {
@@ -64,10 +62,32 @@ console.log(row)
     }
   }
 
-  getCompanyDetails(company:Company){
+  getCompanyDetails(company: Company) {
 
-console.log(company)
+    console.log(company)
 
-this.router.navigate(['/companies-detalis'],{state:company})
+    this.router.navigate(['/companies-detalis'], { state: company })
+  }
+
+
+  addDialog() {
+    this.dialog.open(AddCompanyComponent, {
+      width: '30%'
+    }).afterClosed().subscribe(val => {
+      if (val === 'save') {
+        this.getCompaniesList();
+      }
+    });
+  }
+
+  updateCompany(row : any){
+    this.dialog.open(AddCompanyComponent,{
+      width:'30%',
+      data : row
+    }).afterClosed().subscribe(val =>{
+      if(val==='update'){
+        this.getCompaniesList();
+      }
+  })
   }
 }
