97aa25bcc02cab8588cd6bd8bfb1f0dd97e4b5d6
moretolearn <yanumularammohan661@gmail.com>
2022-06-04 15:40:31 +0530

    Added MyDateAdapter for Date Formart Change

--- stock-market/src/app/app.module.ts
+++ stock-market/src/app/app.module.ts
@@ -16,6 +16,53 @@ import { LoginComponent } from './components/login/login.component';
 import { HeaderComponent } from './components/header/header.component';
 import { DialogComponent } from './components/dialog/dialog.component';
 import { StocksListComponent } from './components/stocks-list/stocks-list.component';
+import { AddStockComponent } from './components/add-stock/add-stock.component';
+import { DateAdapter, MAT_DATE_FORMATS, NativeDateAdapter } from '@angular/material/core';
+import * as moment from 'moment';
+
+export const MY_DATE_FORMATS = {
+  parse: {
+    dateInput: 'DD/MM/YYYY',
+  },
+  display: {
+    dateInput: 'input',
+    monthYearLabel: 'MMMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY'
+  },
+};
+
+export class MyDateAdapter extends NativeDateAdapter {
+
+  override format(date: Date, displayFormat: Object): string {
+    if (displayFormat == "input") {
+      console.log(date)
+      let day = date.getDate();
+      let month = date.getMonth() + 1;
+      let year = date.getFullYear();
+      return this._to2digit(day) + '-' + this._to2digit(month) + '-' + year;
+    } else {
+      return date.toDateString();
+    }
+  }
+
+  override parse(value: any): Date | null {
+    console.log(value)
+    // var date = new Date(value);
+    // let day = date.getDate();
+    // let month = date.getMonth() + 1;
+    // let year = date.getFullYear();
+    // console.log(date)
+    const date = moment(value, 'DD/MM/YYYY');
+
+    return date.isValid() ? date.toDate() : null;
+  }
+
+  private _to2digit(n: number) {
+    return ('00' + n).slice(-2);
+  }
+
+}
 
 @NgModule({
   declarations: [
@@ -27,6 +74,7 @@ import { StocksListComponent } from './components/stocks-list/stocks-list.compon
     HeaderComponent,
     DialogComponent,
     StocksListComponent,
+    AddStockComponent,
   ],
   imports: [
     BrowserModule,
@@ -39,7 +87,9 @@ import { StocksListComponent } from './components/stocks-list/stocks-list.compon
   ],
   providers: [
     { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { floatLabel: 'always' } },
-    { provide: MAT_DIALOG_DEFAULT_OPTIONS, useValue: { hasBackdrop: false } }
+    { provide: MAT_DIALOG_DEFAULT_OPTIONS, useValue: { hasBackdrop: false } },
+    { provide: DateAdapter, useClass: MyDateAdapter },
+    { provide: MAT_DATE_FORMATS, useValue: MY_DATE_FORMATS },
   ],
   bootstrap: [AppComponent]
 })
