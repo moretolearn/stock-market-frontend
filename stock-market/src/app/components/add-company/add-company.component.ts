e37b4ee440fb0b2b7258bf84a02ae8c9db4222ce
moretolearn <yanumularammohan661@gmail.com>
2022-06-05 19:13:44 +0530

    Changed Mat Icons

--- stock-market/src/app/components/add-company/add-company.component.ts
+++ stock-market/src/app/components/add-company/add-company.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { min } from 'moment';
 import { StockmarketService } from 'src/app/services/stockmarket.service';
 
 @Component({
@@ -10,29 +11,29 @@ import { StockmarketService } from 'src/app/services/stockmarket.service';
 })
 export class AddCompanyComponent implements OnInit {
 
-  buttonName : string ="Save";
+  buttonName: string = "Save";
   companyForm !: FormGroup;
   constructor(
-    private formBuilder : FormBuilder,
-    @Inject(MAT_DIALOG_DATA) public editCompanyData:any,
-    private smService : StockmarketService,
-    private dialogRef : MatDialogRef<AddCompanyComponent>) { }
+    private formBuilder: FormBuilder,
+    @Inject(MAT_DIALOG_DATA) public editCompanyData: any,
+    private smService: StockmarketService,
+    private dialogRef: MatDialogRef<AddCompanyComponent>) { }
 
 
 
   ngOnInit(): void {
-    this.companyForm=this.formBuilder.group({
-      companyName : ['',Validators.required],
-      description : ['',Validators.required],
-      ceo : ['',Validators.required],
-      turnover : ['',[Validators.required, Validators.pattern("^[0-9]*$")]],
-      website : ['',Validators.required],
-      exchange : ['', Validators.required]
+    this.companyForm = this.formBuilder.group({
+      companyName: ['', Validators.required],
+      description: ['', Validators.required],
+      ceo: ['', Validators.required],
+      turnover: ['', [Validators.required, Validators.pattern("^[0-9]*$")], Validators.min(100000000)],
+      website: ['', Validators.required],
+      exchange: ['', Validators.required]
 
     })
 
-    if(this.editCompanyData){
-      this.buttonName="Update";
+    if (this.editCompanyData) {
+      this.buttonName = "Update";
       this.companyForm.controls['companyName'].setValue(this.editCompanyData.companyName);
       this.companyForm.controls['description'].setValue(this.editCompanyData.description);
       this.companyForm.controls['ceo'].setValue(this.editCompanyData.ceo);
@@ -43,40 +44,40 @@ export class AddCompanyComponent implements OnInit {
     }
   }
 
-  addCompany(){
-    if(!this.editCompanyData){
-      if(this.companyForm.valid){
+  addCompany() {
+    if (!this.editCompanyData) {
+      if (this.companyForm.valid) {
         this.smService.add(this.companyForm.value)
-        .subscribe({
-          next:(res)=>{
-            alert("Company Registered");
-            this.companyForm.reset();
-            this.dialogRef.close('save');
-          },
-          error:()=>{
-            alert("Error occured while adding the Company")
-          }
+          .subscribe({
+            next: (res) => {
+              alert("Company Registered");
+              this.companyForm.reset();
+              this.dialogRef.close('save');
+            },
+            error: () => {
+              alert("Error occured while adding the Company")
+            }
 
-        })
+          })
       }
     }
-      else{
-        this.updateCompany();
-      }
+    else {
+      this.updateCompany();
     }
+  }
 
-  updateCompany(){
+  updateCompany() {
     this.smService.updateCompany(this.companyForm.value, this.editCompanyData.companyCode)
-    .subscribe({
-      next:(res)=>{
-        alert("Company updated");
-        this.companyForm.reset();
-        this.dialogRef.close('update');
-      },
-      error:()=>{
-        alert("Error occured while updating the Company")
-      }
+      .subscribe({
+        next: (res) => {
+          alert("Company updated");
+          this.companyForm.reset();
+          this.dialogRef.close('update');
+        },
+        error: () => {
+          alert("Error occured while updating the Company")
+        }
 
-    })
+      })
   }
 }
