0f9e6494f9d36b93affa12cc218785a29f78b003
moretolearn <yanumularammohan661@gmail.com>
2022-07-02 19:39:47 +0530

    Added Login Functionality

--- stock-market/src/app/components/login/login.component.ts
+++ stock-market/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { StockmarketService } from 'src/app/services/stockmarket.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm!:FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private smService: StockmarketService,
+    private router:Router) { }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      userName: ['', Validators.required],
+      password: ['', Validators.required],
+    })
+
+
   }
 
+  login(){
+    if(this.loginForm.valid){
+      this.smService.getTokenFromBackEnd(this.loginForm.value).subscribe({
+        next: (res) => {
+          if(res.jwtToken){
+          localStorage.setItem('token',res.jwtToken)
+          this.router.navigate(['/companies'])
+          this.smService.updatemenu.next()
+
+          // console.log(res)
+          // this.smService.getRoles()
+          }
+        },
+      })
+    }
+  }
 }
