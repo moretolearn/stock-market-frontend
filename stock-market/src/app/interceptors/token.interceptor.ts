cccb6144b578eb4fc5b3ac221d6fa68cf33dbdb3
moretolearn <yanumularammohan661@gmail.com>
2022-07-02 19:41:41 +0530

    Added Interceptor

---
+++ stock-market/src/app/interceptors/token.interceptor.ts
@@ -0,0 +1,25 @@
+import { Injectable, Injector } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { StockmarketService } from '../services/stockmarket.service';
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+
+  constructor(private injector:Injector) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    let sm = this.injector.get(StockmarketService);
+    let requestToken=request.clone({
+      setHeaders:{
+        Authorization: ''+sm.getToken()
+      }
+    });
+    return next.handle(requestToken);
+  }
+}
