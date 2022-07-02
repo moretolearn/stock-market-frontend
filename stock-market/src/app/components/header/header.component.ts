e2e70d2d4bcfb58d55a41353676c704efc08e511
moretolearn <yanumularammohan661@gmail.com>
2022-07-02 19:40:31 +0530

    Added Header Functionality

--- stock-market/src/app/components/header/header.component.ts
+++ stock-market/src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { StockmarketService } from 'src/app/services/stockmarket.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private smService:StockmarketService,private router:Router) { }
+
+  isLogin: boolean = false;
 
   ngOnInit(): void {
+    this.smService.updatemenu.subscribe(next=>{
+      console.log(next)
+    })
+  }
+
+  logOut(){
+    localStorage.clear();
+    this.router.navigate(['/login'])
   }
 
 }
