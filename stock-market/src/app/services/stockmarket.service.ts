585573188fdc5332ac647f68dfd7ed6a44ed7899
moretolearn <yanumularammohan661@gmail.com>
2022-07-02 19:41:54 +0530

    Updated stockservice

--- stock-market/src/app/services/stockmarket.service.ts
+++ stock-market/src/app/services/stockmarket.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, map, Observable, throwError } from 'rxjs';
+import { catchError, map, Observable, Subject, throwError } from 'rxjs';
 import { Company } from '../models/company.model';
 import { Stock } from '../models/stock.model';
 import { ApiResponse } from '../models/api-response.model';
@@ -17,25 +17,40 @@ const stockCommand = command + 'stock'
   providedIn: 'root'
 })
 export class StockmarketService {
-  config: NgxUiLoaderConfig
+  config: NgxUiLoaderConfig;
+
+  private _updatemenu = new Subject<void>();
+  get updatemenu() {
+    return this._updatemenu;
+  }
+
   constructor(
     private http: HttpClient,
     private ngxUi: NgxUiLoaderService,
-    private dialog:MatDialog
-    ) { this.config = this.ngxUi.getDefaultConfig() }
+    private dialog: MatDialog
+  ) { this.config = this.ngxUi.getDefaultConfig() }
+
   getAll(): Observable<ApiResponse<Company[]>> {
     this.ngxUi.start();
     return this.http.get(`${companyQuery}/getall`).pipe(map((res: any) => {
       this.ngxUi.stop();
       return res;
-    }));
+    }),
+      catchError(data => {
+        return this.handleServerError(data)
+      })
+    );
   }
   get(id: number): Observable<ApiResponse<Company>> {
     this.ngxUi.start();
     return this.http.get(`${companyQuery}/info/${id}`).pipe(map((res: any) => {
       this.ngxUi.stop();
       return res;
-    }));
+    }),
+    catchError(data => {
+      return this.handleServerError(data)
+    })
+    );
   }
 
   add(company: Company): Observable<ApiResponse<any>> {
@@ -43,7 +58,11 @@ export class StockmarketService {
     return this.http.post(`${companyCommand}/add`, company).pipe(map((res: any) => {
       this.ngxUi.stop();
       return res;
-    }));
+    }),
+    catchError(data => {
+      return this.handleServerError(data)
+    })
+    );
   }
 
   delete(id: number): Observable<ApiResponse<any>> {
@@ -51,7 +70,11 @@ export class StockmarketService {
     return this.http.delete(`${companyCommand}/delete/${id}`).pipe(map((res: any) => {
       this.ngxUi.stop();
       return res;
-    }));
+    }),
+    catchError(data => {
+      return this.handleServerError(data)
+    })
+    );
   }
 
   getCompanyStocksListByCompany(companyCode: number): Observable<ApiResponse<any>> {
@@ -59,7 +82,11 @@ export class StockmarketService {
     return this.http.get(`${companyQuery}/info/${companyCode}`).pipe(map((res: any) => {
       this.ngxUi.stop();
       return res;
-    }));
+    }),
+    catchError(data => {
+      return this.handleServerError(data)
+    })
+    );
   }
 
   getStocksListByCompany(companyCode: number): Observable<ApiResponse<Stock[]>> {
@@ -67,7 +94,11 @@ export class StockmarketService {
     return this.http.get(`${stockQuery}/getall/${companyCode}`).pipe(map((res: any) => {
       this.ngxUi.stop();
       return res;
-    }));
+    }),
+    catchError(data => {
+      return this.handleServerError(data)
+    })
+    );
   }
 
   updateCompany(company: any, id: number): Observable<any> {
@@ -75,7 +106,11 @@ export class StockmarketService {
     return this.http.put<any>(`${companyCommand}/` + id, company).pipe(map((res: any) => {
       this.ngxUi.stop();
       return res;
-    }));
+    }),
+    catchError(data => {
+      return this.handleServerError(data)
+    })
+    );
   }
 
   deleteStock(id: number): Observable<ApiResponse<any>> {
@@ -83,7 +118,11 @@ export class StockmarketService {
     return this.http.delete(`${stockCommand}/${id}`).pipe(map((res: any) => {
       this.ngxUi.stop();
       return res;
-    }));
+    }),
+    catchError(data => {
+      return this.handleServerError(data)
+    })
+    );
   }
 
   addStock(companyCode: number, stock: Stock): Observable<ApiResponse<any>> {
@@ -91,7 +130,11 @@ export class StockmarketService {
     return this.http.post(`${stockCommand}/add/${companyCode}`, stock).pipe(map((res: any) => {
       this.ngxUi.stop();
       return res;
-    }));
+    }),
+    catchError(data => {
+      return this.handleServerError(data)
+    })
+    );
   }
 
   updateStock(stock: any, cid: number, id: number): Observable<any> {
@@ -99,7 +142,11 @@ export class StockmarketService {
     return this.http.put<any>(`${stockCommand}/` + cid + '/' + id, stock).pipe(map((res: any) => {
       this.ngxUi.stop();
       return res;
-    }));
+    }),
+    catchError(data => {
+      return this.handleServerError(data)
+    })
+    );
   }
 
   getStocksByDates(companyCode: number, startDate: string, endDate: string): Observable<any> {
@@ -110,7 +157,24 @@ export class StockmarketService {
           this.ngxUi.stop();
           return res;
         }),
-        catchError(this.handleServerError)
+        catchError(data => {
+          return this.handleServerError(data)
+        })
+      );
+  }
+
+  getTokenFromBackEnd(loginForm: any): Observable<any> {
+    let url = 'http://localhost:9191/end-user/token'
+    this.ngxUi.start();
+    return this.http.post(url, loginForm)
+      .pipe(
+        map((res: any) => {
+          this.ngxUi.stop();
+          return res;
+        }),
+        catchError(data => {
+          return this.handleServerError(data)
+        })
       );
   }
 
@@ -124,8 +188,26 @@ export class StockmarketService {
   }
 
   handleServerError(error: any) {
+    this.ngxUi.stop();
+    this.openDialog(error.message,'red');
     return throwError(() => {
-      return error.error.message;
+      return error.message;
     });
   }
+
+  getToken(): string {
+    return localStorage.getItem('token') || '';
+  }
+
+  getRoles() {
+    let tokenPart = this.getToken().split('.')[1];
+    // let rolesParse = Buffer.from(tokenPart, 'base64').toString('binary');
+    let rolesParse = atob(tokenPart);
+    console.log(rolesParse)
+    // console.log(btoa(rolesParse))
+    let roles = JSON.parse(rolesParse);
+    console.log(roles)
+  }
 }
+
+
